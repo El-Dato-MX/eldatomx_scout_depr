c1b3e7351fcfe7d7acd610c681ffa4b104133318
silverboi <dgplatac@icloud.com>
2024-06-02 19:13:01 -0600

    feature: mod HexBinPlot
    - debug HexBinPlot component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import HexBinPlot from './components/HexBinPlotTwo';
+import PlayerAndTeamLogo from './components/PlayerAndTeamLogo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>NBA Shot Chart HexBin Plot</h1>
-      <HexBinPlot />
+     <h1>NBA Shot Chart HexBin Plot</h1>
+      <div className="logos">
+        <PlayerAndTeamLogo playerId="1626174" teamId="1610612747" />
+      </div>
+      <div className="plot">
+        <HexBinPlot />
+      </div>
     </div>
   );
 }
